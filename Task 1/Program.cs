28e48741ebf4d5c9296a51ee24818263532839ec
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-01 09:03:36 +0400

    finished 1st task out of 15 tasks

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             
+             Task 1:
+             Declare variables to store a person's name (string), age (int), height in meters (double), and whether they are a
+             student (bool). Assign them values directly in the code (no input needed), then print them as a single formatted info
+             card.
+
+             */
+
+            string name = "Shuaib";
+            int age = 24;
+            double height = 173;
+            bool student = false;
+
+            Console.WriteLine("### Person's Info ###");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age + " years old");
+            Console.WriteLine("Height: " + height + " meters");
+            Console.WriteLine("Are you a Student? " + student);
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+
+
         }
     }
 }
